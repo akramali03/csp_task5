1879749f410b1467e926b6f960a1485a27fa626d
akramali03 <143376106+akramali03@users.noreply.github.com>
2023-09-16 14:38:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+console.log("hello world") //output
+
+// this is used for a single line comment
+
+/* this is used for 
+multiline comment */
+
+let number = 10; // Assigned a value to variable "number"
+let ageOfAPerson = 40; // thi is an example of a camelcase
+
+const number5 = 10; //constant variables
+console.log(number5);
+console.log(ageOfAPerson);  
+
+let sample1 = 30;
+let sample2 = 10;
+
+// using the Arithmetic operators.
+
+console.log(sample1 + sample2);
+console.log(sample1 - sample2);
+console.log(sample1 / sample2);
+console.log(sample1 % sample2);
+console.log(sample1 * sample2);
+console.log(sample1 ** sample2);
+
+// using the Assignment operators.
+
+let demo = "hey,";
+demo += "how are you?";
+
+console.log(demo);
+
+let demo1 = 2;
+demo1 -= 1;
+
+console.log(demo1);
+
+let demo2 = 2;
+demo2 *= 3;
+
+console.log(demo2);
+
+
+// using comparison operators.
+
+let demo3 = 3;
+let demo4 = 4;
+
+console.log(demo3 == demo4);
+
+let simple = 4;
+let simple1 = 6;
+
+console.log(simple<simple1);
+
+let simple4 = 6;
+let simple5 = 7;
+
+console.log(simple4<=simple5);
+
+// String methods
+
+let text4 = "SYED AKRAM ALI";
+console.log(text4.length);
+
+let text1 = "hello world";
+console.log(text1.slice(1,5));
+
+let text2 = "Hey There!";
+console.log(text2.substring(1,5));
+
+let example = "American Captain";
+console.log(example.replace("Captain", "Psycho"));
+
+let example2 = "welcome to the class";
+console.log(example2.toUpperCase());
+
+let example3 = "   WELCOME TO MY STUDIO   ";
+console.log(example3.toLowerCase());
+
+console.log(example.concat(" ", example3));
+
+console.log(example3.trim());
